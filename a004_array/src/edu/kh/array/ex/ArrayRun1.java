78163fe3105bcae7b14114c63720a72f18783eb2
kh_work <poer214@naver.com>
2023-02-13 17:31:39 +0900

    230213 branch&array

---
+++ a004_array/src/edu/kh/array/ex/ArrayRun1.java
@@ -0,0 +1,18 @@
+package edu.kh.array.ex;
+
+public class ArrayRun1 {
+
+	public static void main(String[] args) {
+		ArrayEx1 ae = new ArrayEx1();
+		
+//		ae.ex1();
+//		ae.ex2();
+//		ae.ex3();
+//		ae.ex4();
+		ae.ex5();
+		ae.ex6();
+		
+
+	}
+
+}
